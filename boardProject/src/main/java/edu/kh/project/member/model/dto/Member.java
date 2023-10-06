5c32509b7e23fcb8a1888a56d47d6c4b5fc3d7f4
ahnjaeyoun <vnm1994wodbs@gmail.com>
2023-10-06 17:40:04 +0900

    2023 10 06 aca

--- boardProject/src/main/java/edu/kh/project/member/model/dto/Member.java
+++ boardProject/src/main/java/edu/kh/project/member/model/dto/Member.java
@@ -1,17 +1,14 @@
 package edu.kh.project.member.model.dto;
 
-
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
+@NoArgsConstructor
 @Getter
 @Setter
-@NoArgsConstructor
 @ToString
-@AllArgsConstructor
 public class Member {
 	private int memberNo;
 	private String memberEmail;
